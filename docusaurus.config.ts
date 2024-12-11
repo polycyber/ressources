df0af81a03f41a6ba7e3e92c6123849ffbe318d2
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2024-12-10 20:57:59 -0500

    fix config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,15 +20,15 @@ const config: Config = {
   organizationName: 'polycyber', // Usually your GitHub org/user name.
   projectName: 'ressources', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr'],
+    defaultLocale: 'fr-ca',
+    locales: ['fr-ca'],
   },
 
   presets: [
